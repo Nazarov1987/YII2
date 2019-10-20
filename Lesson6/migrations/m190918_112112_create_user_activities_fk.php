f82668192aea2b6345c0057655b69051bbf060e1
Nazarov1987 <45045147+Nazarov1987@users.noreply.github.com>
2019-10-20 21:30:58 +0300

    Домашняя работа №1

--- Lesson6/migrations/m190918_112112_create_user_activities_fk.php
+++
@@ -1,9 +0,0 @@
-<?php
-use yii\db\Migration;
-/**
- * Class m190918_112112_create_user_activities_fk
- */
-class m190918_112112_create_user_activities_fk extends Migration
-{
-    // TODO: связь activity с пользователями
-}
