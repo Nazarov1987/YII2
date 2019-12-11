5f826461a429b3fe967f9fe26f62be6fb8a7cc2e
Nazarov1987 <45045147+Nazarov1987@users.noreply.github.com>
2019-12-11 21:46:36 +0300

    Домашнее задание 7

---
+++ Lesson adv7/backend/config/main.php
@@ -0,0 +1,61 @@
+<?php
+
+use yii\web\UserEvent;
+
+$params = array_merge(
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
+);
+
+return [
+    'id' => 'app-backend',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'backend\controllers',
+    'bootstrap' => ['log'],
+    'layout' => 'admin-lte/main',
+    'modules' => [],
+    'components' => [
+        'request' => [
+            'csrfParam' => '_csrf-backend',
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
+            'on ' . \yii\web\User::EVENT_AFTER_LOGIN => function(UserEvent $event){
+                Yii::info('User logged in with id -' . $event->identity->getId(), 'auth');
+            }
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the backend
+            'name' => 'advanced-backend',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'categories' => ['auth'],
+                    'logFile' => '@runtime/logs/auth.log',
+                    'logVars' => [],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            'showScriptName' => false, // Disable index.php
+            'enablePrettyUrl' => true, // Disable r= routes
+            'rules' => [],
+        ],
+    ],
+    'params' => $params,
+];
